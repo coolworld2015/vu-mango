c039bd4928752827edd03f8f1c78944fb23b3285
coolworld2015 <wintermute2015@ukr.net>
2018-08-23 16:34:38 +0300

    [inputs] - added input-edit

--- src/components/inputs/form-edit.vue
+++ src/components/inputs/form-edit.vue
@@ -1,75 +1,84 @@
 <template>
-	<div v-if="loading">
-		<div style="position: relative; top: 100px; -webkit-box-align:center; -webkit-box-pack:center; display:-webkit-box; font-size:54px">
-			 <img src="../../assets/img/loading.gif">
-		</div>
-	</div>
+  <div v-if="loading">
+    <div style="position: relative; top: 100px; -webkit-box-align:center; -webkit-box-pack:center; display:-webkit-box; font-size:54px">
+      <img src="../../assets/img/loading.gif">
+    </div>
+  </div>
 
-	<div v-else>
-		<div style="margin: auto; width: 25%; font-size: 22px; font-weight: bold; margin-bottom: 10px;">
-			<div style="text-align: center; padding-right: 40px;">{{ name }}</div>
-		</div>
-		<form class="payment-form payment-form--create d-flex justify-content-stretch" autocomplete="off">
-		  <fieldset class="sender-data form-section-wrapper">
-			<div class="form-section" style="width: 100%;">
-			  <div class="form-group">
-				<label for="senderSurname">Name</label>
-				<input type="text" class="form-control" id="senderSurname" placeholder="Name" v-model="name">
-				<div class="invalid-feedback">
-				  Будь ласка, коректно вкажіть прізвище відправника.
-				</div>
-			  </div>
+  <div v-else>
+    <div style="margin: auto; width: 25%; font-size: 22px; font-weight: bold; margin-bottom: 10px;">
+      <div style="text-align: center; padding-right: 40px;">{{ username }}</div>
+    </div>
+    <form class="payment-form payment-form--create d-flex justify-content-stretch" autocomplete="off">
+      <fieldset class="sender-data form-section-wrapper">
+        <div class="form-section" style="width: 100%;">
 
-			  <div class="form-group">
-				<label for="senderName">Phone</label>
-				<input type="text" class="form-control" id="senderName" placeholder="Phone" v-model="phone">
-				<div class="invalid-feedback">
-				  Будь ласка, коректно вкажіть ім'я відправника.
-				</div>
-			  </div>
+          <div class="form-group">
+            <label for="senderPatronymic1">Amount</label>
+            <input type="text" class="form-control" id="senderPatronymic1" placeholder="Street" v-model="amount">
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть по-батькові відправника.
+            </div>
+          </div>
 
-			  <div class="form-group">
-				<label for="senderPatronymic">Street</label>
-				<input type="text" class="form-control" id="senderPatronymic1" placeholder="Street" v-model="street">
-				<div class="invalid-feedback">
-				  Будь ласка, коректно вкажіть по-батькові відправника.
-				</div>
-			  </div>
+          <div class="form-group">
+            <label for="senderPatronymic2">Date</label>
+            <input type="text" class="form-control" id="senderPatronymic2" placeholder="House" v-model="date">
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть по-батькові відправника.
+            </div>
+          </div>
 
-			  <div class="form-group">
-				<label for="senderPatronymic">House</label>
-				<input type="text" class="form-control" id="senderPatronymic1" placeholder="House" v-model="house">
-				<div class="invalid-feedback">
-				  Будь ласка, коректно вкажіть по-батькові відправника.
-				</div>
-			  </div>
+          <div class="form-group">
+            <label for="senderSurname">First Name</label>
+            <input type="text" class="form-control" id="senderSurname" placeholder="Name" v-model="firstName">
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть прізвище відправника.
+            </div>
+          </div>
 
-			  <div class="form-group">
-				<label for="senderPatronymic">Apt</label>
-				<input type="text" class="form-control" id="senderPatronymic1" placeholder="Apt" v-model="apt">
-				<div class="invalid-feedback">
-				  Будь ласка, коректно вкажіть по-батькові відправника.
-				</div>
-			  </div>
+          <div class="form-group">
+            <label for="senderName">Last Name</label>
+            <input type="text" class="form-control" id="senderName" placeholder="Phone" v-model="lastName">
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть ім'я відправника.
+            </div>
+          </div>
 
-			  <div class="form-group">
-				<label for="senderPatronymic">ZIP Code</label>
-				<input type="text" class="form-control" id="senderPatronymic1" placeholder="ZIP Code" v-model="index">
-				<div class="invalid-feedback">
-				  Будь ласка, коректно вкажіть по-батькові відправника.
-				</div>
-			  </div>
+          <div class="form-group">
+            <label for="senderPatronymic3">Username</label>
+            <input type="text" class="form-control" id="senderPatronymic3" placeholder="Apt" v-model="username">
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть по-батькові відправника.
+            </div>
+          </div>
 
-			  <div class="d-flex justify-content-center" style="margin-top: 30px;">
-				<button class="btn btn-danger" v-on:click="goBack" style="margin: 10px; width: 100px; font-size: 14px;">Back</button>
+          <div class="form-group">
+            <label for="senderPatronymic3">E-mail</label>
+            <input type="text" class="form-control" id="senderPatronymic3" placeholder="Apt" v-model="email">
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть по-батькові відправника.
+            </div>
+          </div>
 
-			  </div>
+          <div class="form-group">
+            <label for="senderPatronymic4">ID</label>
+            <input type="text" class="form-control" id="senderPatronymic4" placeholder="ZIP Code" v-model="id">
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть по-батькові відправника.
+            </div>
+          </div>
 
-			</div>
-		  </fieldset>
-		</form>
+          <div class="d-flex justify-content-center" style="margin-top: 30px;">
+            <button class="btn btn-danger" v-on:click="goBack" style="margin: 10px; width: 100px; font-size: 14px;">Back</button>
 
-	</div>
+          </div>
+
+        </div>
+      </fieldset>
+    </form>
+
+  </div>
 </template>
 
 <script>
@@ -100,13 +109,14 @@ export default {
 		setData() {
 			if (appConfig) {
 				if (appConfig.item) {
-					this.id = appConfig.item.id;
-					this.name = appConfig.item.name;
-					this.phone = appConfig.item.phone;
-					this.street = appConfig.item.street;
-					this.house = appConfig.item.house;
-					this.apt = appConfig.item.apt;
-					this.index = appConfig.item.id;
+
+          this.id = appConfig.item.id;
+          this.firstName = appConfig.item.from.first_name;
+          this.lastName = appConfig.item.from.last_name;
+          this.username = appConfig.item.from.username;
+          this.email = appConfig.item.from.email;
+          this.amount = ((+appConfig.item.value).toFixed(2)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+          this.date = appConfig.item.date.split('T')[0] + ' ' + appConfig.item.date.split('T')[1].split('.')[0];
 				}
 			}
 		},
