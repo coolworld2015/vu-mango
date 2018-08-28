52764fee26cb713915491eeacd2ecb5d72088086
coolworld2015 <wintermute2015@ukr.net>
2018-08-28 20:27:52 +0300

    [general] - refactoring

--- src/components/outputs/edit.vue
+++ src/components/outputs/edit.vue
@@ -3,7 +3,7 @@
 		<navbar></navbar>
 		<div class="content">
 			<main>
-				<section class="search-results">
+				<section class="search-results" style="overflow:auto;">
 					<phone-form-edit></phone-form-edit>
 				</section>
 			</main>
